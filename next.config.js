628f6073e959efe89c2bea282bd227c6e57e8c99
mickey2012ex <mickey2012ex@gmail.com>
2019-10-30 10:33:23 +0800

    Deploy Next.js to gh-pages

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    // some configuration
-    assetPrefix: process.env.NODE_ENV === 'production' ? 'https://mickey2012ex.github.io/TestNextDeploy/' : '',
-    // another configuration
-    webpack: (config) => {
-        config.output.publicPath = `/TestNextDeploy${config.output.publicPath}`;
-        return config;
-    },
-}
